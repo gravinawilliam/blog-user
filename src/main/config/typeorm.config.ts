430be003a3a2a8e74db2037dcb036ad31b50d031
gravinawilliam <dev.gravinawilliam@gmail.com>
2021-08-29 16:19:28 -0300

    :sparkles: feat: created typeorm config

---
+++ src/main/config/typeorm.config.ts
@@ -0,0 +1,25 @@
+import envConfig from '@/src/main/config/env.config';
+import { ConnectionOptions } from 'typeorm';
+
+const { dataBaseDefault, nodeEnv } = envConfig;
+
+const dir = nodeEnv === 'TEST' ? 'src' : 'dist/src';
+const extension = nodeEnv === 'TEST' ? 'ts' : 'js';
+
+const typeormConfig: ConnectionOptions = {
+  name: 'default',
+  type: dataBaseDefault.type,
+  host: dataBaseDefault.host,
+  port: dataBaseDefault.port,
+  username: dataBaseDefault.username,
+  password: dataBaseDefault.password,
+  database: dataBaseDefault.database,
+  entities: [`./${dir}/infra/database/typeorm/entities/*.${extension}`],
+  migrations: [`./${dir}/infra/database/typeorm/migrations/*.${extension}`],
+  synchronize: false,
+  cli: {
+    migrationsDir: `./${dir}/infra/database/typeorm/migrations/`,
+  },
+} as ConnectionOptions;
+
+export default typeormConfig;
