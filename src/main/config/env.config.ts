ab102103bf1b1a23653f44b35687561c4444a9a9
gravinawilliam <dev.gravinawilliam@gmail.com>
2021-08-29 15:56:18 -0300

    :sparkles: feat: added envs database default in env config

--- src/main/config/env.config.ts
+++ src/main/config/env.config.ts
@@ -3,6 +3,14 @@ import 'dotenv/config';
 const envConfig = {
   nodeEnv: process.env.NODE_ENV,
   port: process.env.API_PORT || 3000,
+  dataBaseDefault: {
+    type: process.env.DB_DEFAULT_TYPE,
+    host: process.env.DB_DEFAULT_HOST,
+    port: parseInt(process.env.DB_DEFAULT_PORT),
+    username: process.env.DB_DEFAULT_USERNAME,
+    password: process.env.DB_DEFAULT_PASSWORD,
+    database: process.env.DB_DEFAULT_DATABASE,
+  },
 };
 
 export default envConfig;
