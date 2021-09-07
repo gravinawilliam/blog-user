6999fd649882a3dc9e527378fccaab0931bcdf65
gravinawilliam <dev.gravinawilliam@gmail.com>
2021-09-07 17:01:31 -0300

    :recycle: refactor: refactored jest config

---
+++ jest.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from '@jest/types';
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { compilerOptions } = require('./tsconfig.json');
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/application/controllers/**/*.controller.ts',
+    '<rootDir>/src/application/use-cases/**/*.usecase.ts',
+    '<rootDir>/src/application/validators/**/*.validator.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text-summary', 'lcov'],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+};
+
+export default config;
