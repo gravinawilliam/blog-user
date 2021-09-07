7ce80e20527012b49dcc3958694fbb9a49db3805
gravinawilliam <dev.gravinawilliam@gmail.com>
2021-09-07 17:07:42 -0300

    :sparkles: feat: created all exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { AllExceptionsFilter } from '@main/errors/all-exception.filter';
 import { NestFactory } from '@nestjs/core';
 import envConfig from './main/config/env.config';
 import { AppModule } from './main/modules/global/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(envConfig.port || 3000, () => {
     if (envConfig.nodeEnv !== 'PROD') {
       console.log(`✅ OK ${envConfig.port || 3000}`);
---
+++ src/main/errors/all-exception.filter.ts
@@ -0,0 +1,49 @@
+import {
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  Logger,
+  ExceptionFilter,
+} from '@nestjs/common';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  private logger: Logger = new Logger('ExceptionFilter');
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest();
+    const response = ctx.getResponse();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    this.logger.error(exception);
+    console.log('====================================');
+    console.log({
+      error: {
+        statusCode: status,
+        path: request.url,
+        stack: exception.stack,
+        error: exception.error,
+        response:
+          exception instanceof HttpException
+            ? exception.getResponse()
+            : exception,
+      },
+    });
+    console.log('====================================');
+
+    //? Precisa ter esse response pra não ficar dando timeout
+    const res =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : {
+            statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+            message: 'Internal Server Error',
+          };
+
+    response.status(status).json(res);
+  }
+}
