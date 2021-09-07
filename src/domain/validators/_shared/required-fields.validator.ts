6343d38ae34e5df11d5654b242641242f3d7d9ef
gravinawilliam <dev.gravinawilliam@gmail.com>
2021-09-07 17:25:08 -0300

    :sparkles: feat: created interface for required fields validator

---
+++ src/domain/validators/_shared/required-fields.validator.ts
@@ -0,0 +1,9 @@
+import { IRequestRequiredFieldsValidatorDTO } from '@dtos/_shared/request-required-fields.dto';
+import { IHttpResponse } from '@shared/interfaces/http-response.interface';
+import { Either } from '@shared/utils/either';
+
+export interface IRequiredFieldsValidator {
+  execute(
+    data: IRequestRequiredFieldsValidatorDTO,
+  ): Either<IHttpResponse, boolean>;
+}
