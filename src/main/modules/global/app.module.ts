aaef20dac640a81a14f5285fd44b50b3d567d905
gravinawilliam <dev.gravinawilliam@gmail.com>
2021-09-07 18:40:29 -0300

    :sparkles: feat: added module and route of create user in app module

--- src/main/modules/global/app.module.ts
+++ src/main/modules/global/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import typeormConfig from '../../config/typeorm.config';
+import { CreateUserRoute } from '../users/routes/create-user.route';
+import { CreateUserModule } from '../users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(typeormConfig)],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(typeormConfig),
+    CreateUserModule,
+  ],
+  controllers: [CreateUserRoute],
   providers: [],
 })
 export class AppModule {}
