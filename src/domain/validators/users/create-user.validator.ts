39b2c63f106773ee65d4e0a2b02b2704c31de19d
gravinawilliam <dev.gravinawilliam@gmail.com>
2021-09-07 18:28:54 -0300

    :sparkles: feat: created interface for create user validator

---
+++ src/domain/validators/users/create-user.validator.ts
@@ -0,0 +1,7 @@
+import { ICreateUserDTO } from '@domain/dtos/users/create-user.dto';
+import { IHttpResponse } from '@shared/interfaces/http-response.interface';
+import { Either } from '@shared/utils/either';
+
+export interface ICreateUserValidator {
+  execute(createOwner: ICreateUserDTO): Promise<Either<IHttpResponse, boolean>>;
+}
