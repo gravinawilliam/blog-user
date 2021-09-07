6819cbdc24062342aa0568b4e7a0939e3be03f0c
gravinawilliam <dev.gravinawilliam@gmail.com>
2021-09-07 18:08:20 -0300

    :sparkles: feat: created implementation for password validator

---
+++ src/infra/validators/password.validator.ts
@@ -0,0 +1,22 @@
+import { IPasswordValidator } from '@domain/validators/_shared/password.validator';
+import { InvalidParamError } from '@shared/errors/invalid-param.error';
+import { IHttpResponse } from '@shared/interfaces/http-response.interface';
+import { Either, left, right } from '@shared/utils/either';
+import { badRequest } from '@shared/utils/http-response';
+import { validate } from 'secure-password-validator';
+import blackList from 'secure-password-validator/build/main/blacklists/first10_000';
+
+export class PasswordValidator implements IPasswordValidator {
+  async validatePassword(
+    password: string,
+  ): Promise<Either<IHttpResponse, boolean>> {
+    const options = {
+      blackList: blackList,
+    };
+    const { errors, valid } = validate(password, options);
+    if (valid == false) {
+      return left(badRequest(new InvalidParamError(errors.join())));
+    }
+    return right(true);
+  }
+}
