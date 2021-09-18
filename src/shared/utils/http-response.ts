29cc3c2ccb698116642af55225400fcde5062d78
William Gravina <dev.gravinawilliam@gmail.com>
2021-09-17 23:26:17 -0300

    :sparkles: feat: added deleted and notFound in http-response

--- src/shared/utils/http-response.ts
+++ src/shared/utils/http-response.ts
@@ -1,6 +1,6 @@
 import { ServerError } from '@shared/errors/internal-server.error';
-import { IHttpResponse } from '../interfaces/http-response.interface';
-import { HttpStatusCode } from './http-status-code';
+import { IHttpResponse } from '@shared/interfaces/http-response.interface';
+import { HttpStatusCode } from '@shared/utils/http-status-code';
 
 export const badRequest = (error: Error): IHttpResponse => ({
   statusCode: HttpStatusCode.BAD_REQUEST,
@@ -12,12 +12,22 @@ export const serverError = (error: Error): IHttpResponse => ({
   body: new ServerError(error.stack),
 });
 
-export const created = (data: any): IHttpResponse => ({
+export const created = (data: unknown): IHttpResponse => ({
   statusCode: HttpStatusCode.CREATED,
+  body: data,
+});
+
+export const deleted = (data: unknown): IHttpResponse => ({
+  statusCode: HttpStatusCode.OK,
   body: data,
 });
 
 export const conflict = (error: Error): IHttpResponse => ({
   statusCode: HttpStatusCode.CONFLICT,
+  body: error,
+});
+
+export const notFound = (error: Error): IHttpResponse => ({
+  statusCode: HttpStatusCode.NOT_FOUND,
   body: error,
 });
