2d4d3d4d7d63c96acc1fb01b6fd5557b18b6343c
William Gravina <dev.gravinawilliam@gmail.com>
2021-09-23 07:02:33 -0300

    :recycle: refactor: removed password encryption of create user usecase

--- src/application/use-cases/users/create-user.usecase.ts
+++ src/application/use-cases/users/create-user.usecase.ts
@@ -1,4 +1,3 @@
-import { IPasswordEncryption } from '@domain/providers/encryption/password-encryption.provider';
 import { ICreateUserRepository } from '@domain/repositories/users/create-user.repository';
 import { ICreateUserUsecase } from '@domain/use-cases/users/create-user.usecase';
 
@@ -6,20 +5,15 @@ import { ICreateUserResponseDTO } from '@dtos/users/create-user-response.dto';
 import { ICreateUserDTO } from '@dtos/users/create-user.dto';
 
 export class CreateUserUsecase implements ICreateUserUsecase {
-  constructor(
-    private readonly userRepository: ICreateUserRepository,
-    private readonly passwordEncryption: IPasswordEncryption,
-  ) {}
+  constructor(private readonly userRepository: ICreateUserRepository) {}
 
   public async execute({
     email,
     name,
     password,
   }: ICreateUserDTO): Promise<ICreateUserResponseDTO> {
-    const passwordHashed = await this.passwordEncryption.encrypt(password);
-
     const userCreated = await this.userRepository.create({
-      password: passwordHashed,
+      password,
       email,
       name,
     });
