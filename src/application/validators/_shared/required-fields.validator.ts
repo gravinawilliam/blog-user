9e39ac3510cf23dbca380b453cd06f8a30fbbbf9
William Gravina <dev.gravinawilliam@gmail.com>
2021-09-23 06:40:50 -0300

    :recycle: refactor: refactored required fields

--- src/application/validators/_shared/required-fields.validator.ts
+++ src/application/validators/_shared/required-fields.validator.ts
@@ -1,8 +1,10 @@
 import { IRequiredFieldsValidator } from '@domain/validators/_shared/required-fields.validator';
+
 import { IRequestRequiredFieldsValidatorDTO } from '@dtos/_shared/request-required-fields.dto';
+
+import { RequiredParamError } from '@shared/errors/required-param.error';
 import { IHttpResponse } from '@shared/interfaces/http-response.interface';
 import { Either, left, right } from '@shared/utils/either';
-import { RequiredParamError } from '@shared/errors/required-param.error';
 import { badRequest } from '@shared/utils/http-response';
 
 export class RequiredFieldsValidator implements IRequiredFieldsValidator {
@@ -11,13 +13,13 @@ export class RequiredFieldsValidator implements IRequiredFieldsValidator {
     fieldNames,
   }: IRequestRequiredFieldsValidatorDTO): Either<IHttpResponse, boolean> {
     const errors: string[] = [];
-    for (let index = 0; index < fields.length; index++) {
+    for (let index = 0; index < fields.length; index += 1) {
       if (fields[index] == null) {
         errors.push(fieldNames[index]);
       }
     }
 
-    if (errors.length == 0) return right(true);
+    if (errors.length === 0) return right(true);
 
     return left(badRequest(new RequiredParamError(errors.join())));
   }
