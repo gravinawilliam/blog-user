cd65cb891b879f50aafdfe5ce170f32db6050543
William Gravina <dev.gravinawilliam@gmail.com>
2021-09-25 18:30:58 -0300

    :sparkles: feat: added find by user and delete user in fake user repository

--- src/fakes/database/repositories/users.fake.repository.ts
+++ src/fakes/database/repositories/users.fake.repository.ts
@@ -1,11 +1,18 @@
 import { UserModel } from '@domain/models/user.model';
 import { IUuidGenerator } from '@domain/providers/uuidGenerator/uuid-generator.provider';
 import { ICreateUserRepository } from '@domain/repositories/users/create-user.repository';
+import { IDeleteUserRepository } from '@domain/repositories/users/delete-user.repository';
+import { IFindByIdUserRepository } from '@domain/repositories/users/find-by-id-user.repository';
 import { IFindEmailUserRepository } from '@domain/repositories/users/find-email-user.repository';
+
 import { ICreateUserDTO } from '@dtos/users/create-user.dto';
 
 export class FakeUserRepository
-  implements ICreateUserRepository, IFindEmailUserRepository
+  implements
+    ICreateUserRepository,
+    IFindEmailUserRepository,
+    IFindByIdUserRepository,
+    IDeleteUserRepository
 {
   constructor(private readonly uuidGenerator: IUuidGenerator) {}
 
@@ -20,8 +27,19 @@ export class FakeUserRepository
     return userCreated;
   }
 
+  // eslint-disable-next-line consistent-return
+  async delete(id: string): Promise<void> {
+    const findIndex = this.users.findIndex(i => i.id === id);
+    this.users.splice(findIndex, 1);
+  }
+
+  async findById(id: string): Promise<UserModel> {
+    const userFound = this.users.find(user => user.id === id);
+    return userFound;
+  }
+
   async findEmail(email: string): Promise<UserModel> {
-    const userFound = this.users.find((user) => user.email === email);
+    const userFound = this.users.find(user => user.email === email);
     return userFound;
   }
 }
