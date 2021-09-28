a948d021de4c1356f2cff4987db393842459ee50
William Gravina <dev.gravinawilliam@gmail.com>
2021-09-28 09:01:27 -0300

    :sparkles: feat: created implementation for authenticate user controller

---
+++ src/application/controllers/authentications/authenticate-user.controller.ts
@@ -0,0 +1,44 @@
+import { IAuthenticateUserUsecase } from '@domain/use-cases/authentications/authenticate-user.usecase';
+import { IAuthenticateUserValidator } from '@domain/validators/authentications/authenticate-user.validator';
+
+import { IAuthenticateUserResponseDTO } from '@dtos/authentications/authenticate-user-response.dto';
+
+import { IController } from '@shared/interfaces/controller.interface';
+import { IHttpRequest } from '@shared/interfaces/http-request.interface';
+import { IHttpResponse } from '@shared/interfaces/http-response.interface';
+import { ok } from '@shared/utils/http-response';
+
+export class AuthenticateUserController implements IController {
+  constructor(
+    private readonly authenticateUserUseCase: IAuthenticateUserUsecase,
+    private readonly authenticateUserValidator: IAuthenticateUserValidator,
+  ) {}
+
+  async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+    const { email, password } = httpRequest.body;
+
+    const userValidated = await this.authenticateUserValidator.execute({
+      password,
+      email,
+    });
+
+    if (userValidated.isLeft()) {
+      const { body, statusCode } = userValidated.value;
+      return {
+        body,
+        statusCode,
+      };
+    }
+
+    const user = userValidated.value;
+
+    const { token } = await this.authenticateUserUseCase.execute({
+      userId: user.id,
+    });
+
+    return ok({
+      token,
+      user,
+    } as IAuthenticateUserResponseDTO);
+  }
+}
