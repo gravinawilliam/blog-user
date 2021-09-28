3bb04b4004de82a19d9c6bf09b012234e4369094
William Gravina <dev.gravinawilliam@gmail.com>
2021-09-28 08:18:32 -0300

    :sparkles: feat: added cors in main

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { NestFactory } from '@nestjs/core';
 import { AllExceptionsFilter } from '@main/errors/all-exception.filter';
 
 import envConfig from './main/config/env.config';
-import { AppModule } from './main/modules/global/app.module';
+import { AppModule } from './main/modules/_global/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(envConfig.port || 3000, () => {
     if (envConfig.nodeEnv !== 'PROD') {
