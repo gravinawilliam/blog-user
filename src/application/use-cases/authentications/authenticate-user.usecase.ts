92cce59761136fc08a86a66c20695b0bfdab9a7c
William Gravina <dev.gravinawilliam@gmail.com>
2021-10-03 16:42:32 -0300

    :recycle: refactor: refactored authenticate user usecase

--- src/application/use-cases/authentications/authenticate-user.usecase.ts
+++ src/application/use-cases/authentications/authenticate-user.usecase.ts
@@ -1,16 +1,16 @@
-import { ITokenJwtGenerator } from '@domain/providers/token-generator/token-jwt-generator.provider';
+import { ITokenGenerator } from '@domain/providers/token/token-generator.provider';
 import { IAuthenticateUserUsecase } from '@domain/use-cases/authentications/authenticate-user.usecase';
 
 import { IAuthenticateUserUseCaseResponseDTO } from '@dtos/authentications/authenticate-user-usecase-response.dto';
 import { IAuthenticateUserUseCaseDTO } from '@dtos/authentications/authenticate-user-usecase.dto';
 
 export class AuthenticateUserUsecase implements IAuthenticateUserUsecase {
-  constructor(private readonly tokenGenerator: ITokenJwtGenerator) {}
+  constructor(private readonly token: ITokenGenerator) {}
 
   public async execute({
     userId,
   }: IAuthenticateUserUseCaseDTO): Promise<IAuthenticateUserUseCaseResponseDTO> {
-    const token = this.tokenGenerator.jwt(userId);
+    const token = this.token.generate(userId);
 
     return {
       token,
