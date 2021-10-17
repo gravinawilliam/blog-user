ccdf9f4fc9ceb83ddd380735b5d69eb407532d44
William <william.gravina@gcbinvestimentos.com>
2021-10-16 21:40:45 -0300

    :sparkles: feat: added validation login attempts in authenticate user factory

--- src/main/factories/authetications/authenticate-user.factory.ts
+++ src/main/factories/authetications/authenticate-user.factory.ts
@@ -1,8 +1,10 @@
 import { AuthenticateUserController } from '@application/controllers/authentications/authenticate-user.controller';
 import { AuthenticateUserUsecase } from '@application/use-cases/authentications/authenticate-user.usecase';
 import { RequiredFieldsValidator } from '@application/validators/_shared/required-fields.validator';
+import { ValidationLoginAttemptsValidator } from '@application/validators/_shared/validation-login-attempts.validator';
 import { AuthenticateUserValidator } from '@application/validators/authentications/authenticate-user.validator';
 
+import UserAccessLogTypeormRepository from '@infra/database/typeorm/repositories/user-access-log-typeorm.repository';
 import UsersTypeormRepository from '@infra/database/typeorm/repositories/users-typeorm.repository';
 import { ComparePasswordEncrypted } from '@infra/providers/encryption/compare-password-encrypted.provider';
 import { TokenJwt } from '@infra/providers/token-jwt/token-jwt.provider';
@@ -14,11 +16,17 @@ export const makeAuthenticateUserController =
     const usersRepository = new UsersTypeormRepository();
     const emailValidator = new EmailValidator();
     const comparePasswordEncrypted = new ComparePasswordEncrypted();
+    const userAccessLogRepository = new UserAccessLogTypeormRepository();
+    const validationLoginAttempts = new ValidationLoginAttemptsValidator(
+      userAccessLogRepository,
+    );
     const authenticateUserValidator = new AuthenticateUserValidator(
       requiredFieldsValidator,
       usersRepository,
       emailValidator,
       comparePasswordEncrypted,
+      validationLoginAttempts,
+      userAccessLogRepository,
     );
     const tokenGenerator = new TokenJwt();
     const authenticateUserUsecase = new AuthenticateUserUsecase(tokenGenerator);
