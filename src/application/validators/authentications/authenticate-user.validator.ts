e835d60528c7efd440084182b47c769f78cdf544
William <william.gravina@gcbinvestimentos.com>
2021-10-16 21:38:00 -0300

    :sparkles: feat: implemented validation login attempts in authenticate user validator

--- src/application/validators/authentications/authenticate-user.validator.ts
+++ src/application/validators/authentications/authenticate-user.validator.ts
@@ -1,11 +1,15 @@
 import { IComparePasswordEncrypted } from '@domain/providers/encryption/compare-password-encrypted.provider';
+import { ICreateFailureUserAccessLogRepository } from '@domain/repositories/user-access-log/create-failure-user-access-log.repository';
 import { IFindEmailUserRepository } from '@domain/repositories/users/find-email-user.repository';
 import { IEmailValidator } from '@domain/validators/_shared/email.validator';
 import { IRequiredFieldsValidator } from '@domain/validators/_shared/required-fields.validator';
+import { IValidationLoginAttemptsValidator } from '@domain/validators/_shared/validation-login-attempts.validator';
 import { IAuthenticateUserValidator } from '@domain/validators/authentications/authenticate-user.validator';
 
 import { IAuthenticateUserValidatorResponseDTO } from '@dtos/authentications/authenticate-user-validator-response.dto';
 import { IAuthenticateUserDTO } from '@dtos/authentications/authenticate-user.dto';
+
+import { authConfig } from '@main/config/auth.config';
 
 import { InvalidParamError } from '@shared/errors/invalid-param.error';
 import { NotFoundModelError } from '@shared/errors/not-found-model.error';
@@ -19,6 +23,8 @@ export class AuthenticateUserValidator implements IAuthenticateUserValidator {
     private readonly usersRepository: IFindEmailUserRepository,
     private readonly emailValidator: IEmailValidator,
     private readonly comparePasswordEncrypted: IComparePasswordEncrypted,
+    private readonly validationLoginAttempts: IValidationLoginAttemptsValidator,
+    private readonly userAccessLogRepository: ICreateFailureUserAccessLogRepository,
   ) {}
 
   public async execute({
@@ -43,6 +49,13 @@ export class AuthenticateUserValidator implements IAuthenticateUserValidator {
       return left(notFound(new NotFoundModelError('user')));
     }
 
+    const attemptsValidated = await this.validationLoginAttempts.execute({
+      conditionsTimeAttempts: authConfig.conditionsTimeAttempts,
+      userId: user.id,
+      tryLimit: authConfig.tryLimit,
+    });
+    if (attemptsValidated.isLeft()) return left(attemptsValidated.value);
+
     const passwordMatched = await this.comparePasswordEncrypted.comparePassword(
       {
         password,
@@ -50,6 +63,9 @@ export class AuthenticateUserValidator implements IAuthenticateUserValidator {
       },
     );
     if (passwordMatched === false) {
+      await this.userAccessLogRepository.createFailure({
+        userId: user.id,
+      });
       return left(badRequest(new InvalidParamError('password')));
     }
 
@@ -58,11 +74,4 @@ export class AuthenticateUserValidator implements IAuthenticateUserValidator {
       email: user.email,
     });
   }
-
-  // public async execute({
-  //   email,
-  //   password,
-  // }: IAuthenticateUserDTO): Promise<Either<IHttpResponse, boolean>> {
-
-  // }
 }
