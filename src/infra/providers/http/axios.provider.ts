f5107a4dae0ff03776008fa3aeefef35f8b18fb6
William <william.gravina@gcbinvestimentos.com>
2021-11-07 20:59:30 -0300

    refactor: refactored axios provider

--- src/infra/providers/http/axios.provider.ts
+++ src/infra/providers/http/axios.provider.ts
@@ -1,20 +1,41 @@
 import axios from 'axios';
 
-import { IPostHttp } from '@domain/providers/http/http.provider';
+import { IPostHttp } from '@domain/providers/http/post-http.provider';
+import { IPutHttp } from '@domain/providers/http/put-http.provider';
 
 import {
   IParamsPostHttpDTO,
   IResponseHttpResponseDTO,
 } from '@dtos/providers/http/http-provider.dto';
 
-export class AxiosHttpProvider implements IPostHttp {
+export class AxiosHttpProvider implements IPostHttp, IPutHttp {
   public async post({
     data,
     url,
   }: IParamsPostHttpDTO): Promise<IResponseHttpResponseDTO> {
-    const response = await axios.post(url, data);
-    return {
-      response,
-    };
+    try {
+      return await axios.post(url, data);
+    } catch (error) {
+      // TODO: adicionar aqui o envio do erro para o serviço de log
+      console.log(error);
+      return {
+        response: error.response,
+      };
+    }
+  }
+
+  public async put({
+    data,
+    url,
+  }: IParamsPostHttpDTO): Promise<IResponseHttpResponseDTO> {
+    try {
+      return await axios.put(url, data);
+    } catch (error) {
+      // TODO: adicionar aqui o envio do erro para o serviço de log
+      console.log(error);
+      return {
+        response: error.response,
+      };
+    }
   }
 }
