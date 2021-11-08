b75da30e8cdb272b7235a8bef809ed966b5ab31c
William <william.gravina@gcbinvestimentos.com>
2021-11-07 21:38:29 -0300

    feat: implemented interface update user in user data replication

--- src/infra/providers/data-replications/users/user-data-replication.provider.ts
+++ src/infra/providers/data-replications/users/user-data-replication.provider.ts
@@ -1,17 +1,28 @@
 import { ICreateUserDataReplication } from '@domain/providers/data-replications/users/create-user-data-replication.provider';
-import { IPostHttp } from '@domain/providers/http/http.provider';
+import { IUpdateUserDataReplicationProvider } from '@domain/providers/data-replications/users/update-user-data-replication.provider';
+import { IPostHttp } from '@domain/providers/http/post-http.provider';
+import { IPutHttp } from '@domain/providers/http/put-http.provider';
 
 import { UserModel } from '@models/user.model';
 
 import { envConfig } from '@main/config/env.config';
 
-export class UserDataReplication implements ICreateUserDataReplication {
-  constructor(private readonly httpRequest: IPostHttp) {}
+export class UserDataReplication
+  implements ICreateUserDataReplication, IUpdateUserDataReplicationProvider
+{
+  constructor(private readonly httpRequest: IPostHttp & IPutHttp) {}
 
   public async createUser(user: UserModel): Promise<void> {
     await this.httpRequest.post({
       data: user,
       url: `${envConfig.url.internalMicroServices.dataReplication}/users/replication/create`,
+    });
+  }
+
+  public async updateUser(user: UserModel): Promise<void> {
+    await this.httpRequest.put({
+      data: user,
+      url: `${envConfig.url.internalMicroServices.dataReplication}/users/replication/update`,
     });
   }
 }
