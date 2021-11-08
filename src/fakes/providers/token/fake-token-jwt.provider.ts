a4765c4f9de991c2524e1d9e4e78adec35804230
William <william.gravina@gcbinvestimentos.com>
2021-11-07 21:36:16 -0300

    feat: implemented interface token verify in fake token jwt

--- src/fakes/providers/token/fake-token-jwt.provider.ts
+++ src/fakes/providers/token/fake-token-jwt.provider.ts
@@ -1,6 +1,29 @@
 import { ITokenGenerator } from '@domain/providers/token/token-generator.provider';
+import { ITokenVerify } from '@domain/providers/token/token-verify.provider';
 
-export class FakeTokenJwt implements ITokenGenerator {
+import {
+  IRequestTokenVerify,
+  IResponseTokenVerify,
+} from '@dtos/providers/tokens/tokens.dto';
+
+import { UnauthorizedError } from '@shared/errors/unauthorized.error';
+import { IHttpResponse } from '@shared/interfaces/http-response.interface';
+import { Either, left, right } from '@shared/utils/either';
+import { unauthorized } from '@shared/utils/http-response';
+
+export class FakeTokenJwt implements ITokenGenerator, ITokenVerify {
+  verify({
+    authorization,
+  }: IRequestTokenVerify): Either<IHttpResponse, IResponseTokenVerify> {
+    const [, token] = authorization.split(' ');
+    if (token === 'invalid') {
+      return left(unauthorized(new UnauthorizedError('Invalid JWT token')));
+    }
+    return right({
+      userId: token,
+    });
+  }
+
   generate(userId: string): string {
     return `Bearer ${userId}`;
   }
