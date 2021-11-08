e94eef7bc801bb4f770f2a9be37b4a132281dec1
William <william.gravina@gcbinvestimentos.com>
2021-11-07 21:37:14 -0300

    feat: implemented interface update user in fake data replications repository

--- src/fakes/replications/replications.repository.ts
+++ src/fakes/replications/replications.repository.ts
@@ -1,9 +1,10 @@
 import { ICreateUserDataReplication } from '@domain/providers/data-replications/users/create-user-data-replication.provider';
+import { IUpdateUserDataReplicationProvider } from '@domain/providers/data-replications/users/update-user-data-replication.provider';
 
 import { UserModel } from '@models/user.model';
 
 export class FakeDataReplicationsRepository
-  implements ICreateUserDataReplication
+  implements ICreateUserDataReplication, IUpdateUserDataReplicationProvider
 {
   private users: UserModel[] = [];
 
@@ -12,5 +13,12 @@ export class FakeDataReplicationsRepository
   public async createUser(user: UserModel): Promise<void> {
     const userCreated = Object.assign(new UserModel(), user);
     this.users.push(userCreated);
+  }
+
+  // ? I disabled the rule because here is an interface implementation
+  // eslint-disable-next-line require-await
+  async updateUser(user: UserModel): Promise<void> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+    this.users[findIndex] = user;
   }
 }
