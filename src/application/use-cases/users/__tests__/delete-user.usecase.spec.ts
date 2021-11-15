7be53ff34a0277793df1a7de9f909c1371dd14fc
William Gravina <dev.gravinawilliam@gmail.com>
2021-11-15 14:56:44 -0300

    test: refactor delete user usecase

--- src/application/use-cases/users/__tests__/delete-user.usecase.spec.ts
+++ src/application/use-cases/users/__tests__/delete-user.usecase.spec.ts
@@ -1,21 +1,28 @@
+import { IUserDataReplication } from '@domain/providers/data-replications/users/user-data-replication.provider';
 import { ICreateUserRepository } from '@domain/repositories/users/create-user.repository';
 import { IDeleteUserRepository } from '@domain/repositories/users/delete-user.repository';
 import { IDeleteUserUseCase } from '@domain/use-cases/users/delete-user.usecase';
 
 import { FakeUserRepository } from '@fakes/database/repositories/users-fake.repository';
 import { FakeUuidGenerator } from '@fakes/providers/uuid/uuid-generator.fake.provider';
+import { FakeDataReplicationsRepository } from '@fakes/replications/replications.repository';
 
 import { DeleteUserUseCase } from '../delete-user.usecase';
 
 let deleteUserUseCase: IDeleteUserUseCase;
 let fakeUsersRepository: IDeleteUserRepository & ICreateUserRepository;
 let fakeUuidGenerator: FakeUuidGenerator;
+let fakeDataReplications: IUserDataReplication;
 
 describe('DeleteUserUseCase', () => {
   beforeEach(() => {
     fakeUuidGenerator = new FakeUuidGenerator();
     fakeUsersRepository = new FakeUserRepository(fakeUuidGenerator);
-    deleteUserUseCase = new DeleteUserUseCase(fakeUsersRepository);
+    fakeDataReplications = new FakeDataReplicationsRepository();
+    deleteUserUseCase = new DeleteUserUseCase(
+      fakeUsersRepository,
+      fakeDataReplications,
+    );
   });
 
   it('should be able to delete user', async () => {
@@ -25,7 +32,7 @@ describe('DeleteUserUseCase', () => {
       password: '3k8EG923iA',
     });
     await deleteUserUseCase.execute({
-      id: user.id,
+      user,
     });
   });
 });
